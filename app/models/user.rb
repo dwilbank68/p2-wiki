df06c8279cb61938816474665837d094d174fe4e
David Wilbanks <dwilbank@gmail.com>
2014-10-21 22:53:24 -0700

    added pundit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    role == 'admin'          # which is really self.role == 'admin'
+  end
+
+  def moderator?
+    role == 'moderator'
+  end
+
+
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update, :show, :index]
 
-  resources 'wikis'
+  resources :wikis
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
