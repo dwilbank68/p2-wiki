40dcd48d1038712cc9a1f27477374cede33ab745
David Wilbanks <dwilbank@gmail.com>
2014-12-02 16:12:37 -0800

    removed duplicate migrations

--- db/migrate/20141120192413_add_account_expiration_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAccountExpirationToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :account_expiration, :datetime
-  end
-end
--- db/migrate/20141120192803_add_account_activation_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAccountActivationToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :account_activation, :datetime
-  end
-end
