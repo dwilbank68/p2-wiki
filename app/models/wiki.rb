7695f085a8b6f15572978329a319aed1a739c317
David Wilbanks <dwilbank@gmail.com>
2015-03-25 09:09:30 -0700

    Update wiki.rb

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,22 +1,7 @@
-# == Schema Information
-#
-# Table name: wikis
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  body       :text
-#  private    :boolean          default(FALSE)
-#  user_id    :integer
-#  created_at :datetime
-#  updated_at :datetime
-#  slug       :string(255)
-#
-
 class Wiki < ActiveRecord::Base
 
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
-  #friendly_id :name, :use => :history
 
   belongs_to  :user
   has_many    :collaborations
