dc1e5d0792057dad670ffaf122cbe72b7dc3dedf
David Wilbanks <dwilbank@gmail.com>
2015-03-25 09:09:08 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,10 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :integer          not null, primary key
-#  name                   :string(255)
-#  role                   :string(255)
-#  email                  :string(255)      default(""), not null
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  subscribed             :boolean          default(FALSE)
-#  stripeid               :string(255)
-#  created_at             :datetime
-#  updated_at             :datetime
-#  account_expiration     :datetime
-#  account_activation     :datetime
-#
-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :wikis # that he created
+  has_many :wikis
   has_many :collaborations
   has_many :wiki_contributions, :through => :collaborations, :source => :wiki
 
@@ -38,7 +13,7 @@ class User < ActiveRecord::Base
   scope :all_except, ->(user) { where.not(id:user) }
 
   def admin?
-    role == 'admin'          # which is really self.role == 'admin'
+    role == 'admin'
   end
 
 
@@ -56,7 +31,3 @@ class User < ActiveRecord::Base
 
 end
 
-# user.wikis
-# user.wiki_contributions
-# wiki.user
-# wiki.users
