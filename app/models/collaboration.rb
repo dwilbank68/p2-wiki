bc53e6ee34d32985a69dd8c55db552f3bdf8be99
David Wilbanks <dwilbank@gmail.com>
2015-03-25 09:08:31 -0700

    Update collaboration.rb

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: collaborations
-#
-#  id         :integer          not null, primary key
-#  user_id    :integer
-#  wiki_id    :integer
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class Collaboration < ActiveRecord::Base
   belongs_to :user
   belongs_to :wiki
